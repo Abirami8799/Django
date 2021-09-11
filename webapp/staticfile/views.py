3472e6a742917c8bce081d4ba83af793acd1de35
Abirami8799 <mabirami8799@gmail.com>
2021-09-11 10:28:25 +0530

    basics

---
+++ webapp/staticfile/views.py
@@ -0,0 +1,21 @@
+from staticfile.models import Destination
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Destination
+
+# Create your views here.
+def travello(request):
+
+    # dest1= Destination()
+    
+    # dest1.name= 'chennai'
+    # dest1.price= 800
+    # dest1.desc= 'TamilNadu captial'
+
+    return render(request,'index.html',{'dest1':'Chennai'})
+
+
+
+
+
+
