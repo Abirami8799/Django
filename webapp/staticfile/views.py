264f90d54a2cfa2aa54e48e3108e29c377806ab1
Abirami8799 <mabirami8799@gmail.com>
2021-09-11 13:33:54 +0530

    basics

--- webapp/staticfile/views.py
+++ webapp/staticfile/views.py
@@ -8,11 +8,27 @@ def travello(request):
 
     dest1= Destination()
     
-    dest1.name= 'chennai'
-    dest1.price= 800
-    dest1.desc= 'TamilNadu captial'
+    dest1.name= 'TamilNadu'
+    dest1.price= 750
+    dest1.desc= 'Shore Temple, Mahabalipuram'
+    dest1.img='destination_1.jpg'
 
-    return render(request,'index.html',{'dest1':dest1})
+
+    dest2= Destination()
+    dest2.name= 'Kerala'
+    dest2.price= 700
+    dest2.desc= ' Edakkal Caves, Wayanad'
+    dest2.img='destination_2.jpg'
+
+    dest3= Destination()
+    dest3.name= ' Karnataka'
+    dest3.price= 800
+    dest3.desc= 'Mysore Palace, Mysore'
+    dest3.img='destination_3.jpg'
+
+    dests=[dest1,dest2, dest3]
+
+    return render(request,'index.html',{'dests':dests})
 
 
 
